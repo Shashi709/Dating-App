712c3869521420643f046ad1d7b87e54f9bef3c6
Shashi709 <shashimsi63@gmail.com>
2023-07-21 11:47:44 +0530

    Client login and register

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model:any = {}
+  currentUser$:Observable<User | null> = of(null);
+
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    // this.getCurrentUser();
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+
+
+}
